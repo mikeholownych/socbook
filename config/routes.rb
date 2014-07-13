1902a46fa8911f79d2d4234d1fccaf33366812d8
Mike Holownych <mike.holownych@gmail.com>
2014-07-13 15:35:29 +0000

    Added Facebook Auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Socbook::Application.routes.draw do
 
   resources :topics
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
   get "pages/index"
   get "pages/about"
 
-  root :to => "pages#index"
+  root to: 'pages#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713141625) do
+ActiveRecord::Schema.define(version: 20140713152605) do
 
   create_table "likes", force: true do |t|
     t.integer  "user_id"
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(version: 20140713141625) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
