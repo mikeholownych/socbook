75cb15f26d376c2c81103320fc16b737b0143a41
Mike Holownych <mike.holownych@gmail.com>
2014-07-19 03:27:01 +0000

    bugfix for production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Socbook::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host 'socbook.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'socbook.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
